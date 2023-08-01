c12871a5089e4d92dbd0b3519fb2210b3f89c4a1
Cade <134173296+LavenderPeach@users.noreply.github.com>
2023-07-31 20:16:03 -0400

    Create JS fundamentals
    Provided context for canvas and established grid size as well as the size of the grid cells.

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+// Size of each grid cell
+
+const gridCellSize = 20;
+
+// Calc # of cells
+
+const gridSizeX = math.floor(canvas.width / gridCellSize);
+const gridSizeY = math.floor(canvas.height / gridCellSize);
